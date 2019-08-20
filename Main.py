456e50345ee1a1fcc67abf527172a2a8f69d94aa
Stav Cohen <cohenstav1@gmail.com>
2019-08-20 15:30:16 +0300

    Adding Cycle Check and Reverse Function to Singly Linked Last

--- Main.py
+++ Main.py
@@ -5,19 +5,24 @@ from DoublyLinkedList.DoublyLinkedList import *
 from DoublyLinkedList.DoublyLinkedListNode import *
 def main():
 ########## testing single linked list
-   single_linked_list=SingleyLinkedList(Node(1))
-   single_linked_list.add_node(Node(2))
-   single_linked_list.add_node(Node(3))
-   single_linked_list.add_node(Node(4))
-   single_linked_list.print()
-   print(single_linked_list.tail.get_value())
+    single_linked_list=SingleyLinkedList(Node(1))
+    single_linked_list.add_node(Node(2))
+    single_linked_list.add_node(Node(3))
+    single_linked_list.add_node(Node(4))
+    single_linked_list.print()
+    print(single_linked_list.tail.get_value())
+    print(single_linked_list.cycle_check())
+   # single_linked_list.tail.next_node=single_linked_list.tail
+   # print(single_linked_list.cycle_check())
+    single_linked_list.reverse_list()
+    single_linked_list.print()
 ###########
 
 ########### testing double linked list
-   double_linked_list=DoublyLinkedList(DoublyLinkedListNode(1))
-   double_linked_list.add_node(DoublyLinkedListNode(2))
-   double_linked_list.add_node(DoublyLinkedListNode(3))
-   print(double_linked_list.tail.prev_node.prev_node.get_value())
+  # double_linked_list=DoublyLinkedList(DoublyLinkedListNode(1))
+  # double_linked_list.add_node(DoublyLinkedListNode(2))
+  # double_linked_list.add_node(DoublyLinkedListNode(3))
+  # print(double_linked_list.tail.prev_node.prev_node.get_value())
 ###########
 if __name__ == '__main__':
     main()
--- SinglyLinkedList/SinglyLinkedList.py
+++ SinglyLinkedList/SinglyLinkedList.py
@@ -1,17 +1,39 @@
 class SingleyLinkedList(object):
 
-    def __init__(self,node):
-        self.head=node
-        self.tail=node
+    def __init__(self, node):
+        self.head = node
+        self.tail = node
 
-    def add_node(self,node):
-        self.tail.next_node=node
-        self.tail=node
+    def add_node(self, node):
+        self.tail.next_node = node
+        self.tail = node
 
     def print(self):
-        curr_node=self.head
-        while curr_node!= None:
+        curr_node = self.head
+        while curr_node != None:
             print(curr_node.get_value())
-            curr_node=curr_node.get_next()
+            curr_node = curr_node.get_next()
 
+    def cycle_check(self):
+        curr_node = self.head
+        while curr_node != None:
+            if curr_node == curr_node.get_next():
+                return True
+            curr_node = curr_node.get_next()
+        return False
 
+    def reverse_list(self):
+        current = self.head
+        previous = None
+        next = None
+
+        while current != None:
+            next = current.next_node
+            current.next_node = previous
+            previous = current
+            current = next
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+        return previous
